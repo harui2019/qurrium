fc59711cb54863c6a74775516e15453b815f549a
harui <james880818@gmail.com>
2022-06-15 23:48:34 +0800

    feat: remove qurrium/mori backup

--- qurrium/backup/__init__.py
+++
@@ -1,5 +0,0 @@
-from .attrdict import argdict, argTuple
-from .jsonablize import Parse as jsonablize, quickJSONExport, keyTupleLoads
-from .configuration import Configuration
-from .gitsync import syncControl
-from .tagmaps import TagMap
