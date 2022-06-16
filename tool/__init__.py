fb4fddf91c2e5347a0625a5f8250ca379f5e1a4c
harui <james880818@gmail.com>
2022-06-16 11:41:50 +0800

    feat: qurecipe introduce

--- tool/__init__.py
+++ tool/__init__.py
@@ -1,17 +1,5 @@
 from .draw import *
 from .command import *
-# Mori
-# try:
-#     from .mori.attrdict import argdict
-#     from .mori.jsonablize import Parse as jsonablize, quickJSONExport, keyTupleLoads
-#     from .mori.configuration import Configuration
-#     from .mori.gitsync import syncControl
-# except:
-#     from .backup.attrdict import argdict
-#     from .backup.jsonablize import Parse as jsonablize, quickJSONExport, keyTupleLoads
-#     from .backup.configuration import Configuration
-#     from .backup.gitsync import syncControl
-# Gajima
 try:
     from .gajima.loading import Gajima
 except:
