32ef829de90bfdc443d25703fc53ad5af8c2b1dc
harui <james880818@gmail.com>
2022-06-24 11:19:12 +0800

    fix: better import strategy on test

--- tests/test_qurstrop.py
+++ tests/test_qurstrop.py
@@ -3,36 +3,36 @@ from qiskit import (
     QuantumRegister, ClassicalRegister, QuantumCircuit,
 )
 import pytest
-
-import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
-import qurry
-
-# from qurry.case import trivialParamagnet
 from qurry.qurstrop import StringOperator
+
+expDemo01 = StringOperator()
+try:
+    from qurry.case import trivialParamagnet
+
+    wave_adds = [
+        (expDemo01.addWave(trivialParamagnet(i).wave(), i),) for i in range(6, 12, 2)
+    ]
+except:
+    def trivialParamagnet(n) -> QuantumCircuit:
+        """Construct the example circuit.
+
+        Returns:
+            QuantumCircuit: The example circuit.
+        """
+        q = QuantumRegister(n, "q")
+        qc = QuantumCircuit(q)
+        [qc.h(q[i]) for i in range(n)]
+    
+        return qc
+    wave_adds = [
+        (expDemo01.addWave(trivialParamagnet(i), i),) for i in range(6, 12, 2)
+    ]
+
 
 backend = {
     'aer': Aer.get_backend('aer_simulator'),
 }
 
-def trivialParamagnet(n) -> QuantumCircuit:
-    """Construct the example circuit.
-
-    Returns:
-        QuantumCircuit: The example circuit.
-    """
-    q = QuantumRegister(n, "q")
-    qc = QuantumCircuit(q)
-    [qc.h(q[i]) for i in range(n)]
-    
-    return qc
-
-expDemo01 = StringOperator()
-wave_adds = [
-    # (expDemo01.addWave(trivialParamagnet(i).wave(), i),) for i in range(6, 12, 2)
-    (expDemo01.addWave(trivialParamagnet(i), i),) for i in range(6, 12, 2)
-]
 
 @pytest.mark.parametrize("tgt, ", wave_adds)
 def test_quantity(
