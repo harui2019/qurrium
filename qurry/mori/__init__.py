39c30f3bad99338a52c10d12e4ea8c7abd3058b4
harui <james880818@gmail.com>
2022-07-04 20:03:11 +0800

    feat: subtree mori drop

--- qurry/mori/__init__.py
+++
@@ -1,7 +0,0 @@
-from .argrecord import argdict, argTuple
-from .jsonablize import Parse as jsonablize, quickJSONExport, sortHashableAhead
-from .configuration import Configuration
-from .gitsync import syncControl
-
-from .tagmaps.tagmaps import TagMap, keyTupleLoads, tupleStrParse
-from .tagmaps.quantity import quantitiesMean, tagMapQuantityMean, Q
