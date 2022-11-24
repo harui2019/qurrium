6b1c4e4f92f1fa89f8e33b4fed4ace4fde95ea78
harui <james880818@gmail.com>
2022-11-24 13:08:05 +0800

    fix(github.action)

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ with open(README_PATH) as readme_file:
     
 requirement = [
     "qiskit>=0.35.0",
-    "qiskit-aer-gpu;platform_system=='Linux';python_version<='3.9'",
+    "qiskit-aer-gpu; platform_system=='Linux'",
     "tqdm",
     "dask",
+    "matplotlib",
 ]
 
 __author__ = "Huai-Chung Chang (harui2019@proton.me)"
