bfcc238c32d8f1f8cd3fb25e54fdb65d58d1bfe6
harui <harui2019@proton.me>
2023-03-08 01:09:19 +0800

    chore: auto version tag create

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-from qurry import __version__, __version_str__
+from qurry.version import __version__, __version_str__
 
 README_PATH = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), 'README.md')
