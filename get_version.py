7d71c7c2cec4bc618d60d602566ae75764f8bc8b
harui <harui2019@proton.me>
2023-03-08 02:16:22 +0800

    fix: tagcname

--- get_version.py
+++ get_version.py
@@ -6,7 +6,7 @@ ver_path = convert_path('./qurry/version.py')
 with open(ver_path) as ver_file:
     exec(ver_file.read(), main_ns)
 
-__version_str__ = main_ns['__version_str__']
-print(f'| Version: v{__version_str__}')
+__version_str__ = 'v'+main_ns['__version_str__']
+print(f'| Version: {__version_str__}')
 # 將版本號碼傳遞到環境變數中
 os.system(f'echo "VERSION={__version_str__}" >> $GITHUB_ENV')
