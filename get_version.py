474ba63ba6f4a11660f66150234a852f1470a067
harui <harui2019@proton.me>
2023-03-08 01:42:25 +0800

    chore: auto version tag create

--- get_version.py
+++ get_version.py
@@ -1,6 +1,11 @@
-from .qurry import __version_str__
+from distutils.util import convert_path
 
-print(f'The version number is {__version_str__}')
+main_ns = {}
+ver_path = convert_path('./qurry/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
+__version_str__ = main_ns['__version_str__']
+print(f'| Version: {__version_str__}')
 # 將版本號碼傳遞到環境變數中
 print(f'::set-env name=VERSION::{__version_str__}')
