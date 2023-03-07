21febb2e63e2c534f51c62d8acdbf30c45cb8717
harui <harui2019@proton.me>
2023-03-08 01:02:03 +0800

    chore: auto version tag create

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os
 from setuptools import setup, find_packages
 
-from .qurry import __version__, __version_str__
+from qurry import __version__, __version_str__
 
 README_PATH = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), 'README.md')
 with open(README_PATH) as readme_file:
     README = readme_file.read()
-    
+
 qiskit_main = [
     "qiskit==0.39.2",
     "qiskit-terra==0.22.2",
@@ -15,7 +15,8 @@ qiskit_main = [
     "qiskit-ibmq-provider==0.19.2",
 ]
 qiskit_gpu = [
-    "qiskit-aer-gpu; platform_system=='Linux' and python_version<='3.9'", # https://peps.python.org/pep-0508/
+    # https://peps.python.org/pep-0508/
+    "qiskit-aer-gpu; platform_system=='Linux' and python_version<='3.9'",
 ]
 bugfix = [
     "urllib3==1.22",
@@ -36,14 +37,14 @@ setup(
     description='Qurry 🍛 - The Measuring Tool for Renyi Entropy, Loschmidt Echo, and Magnetization Squared, The Library of Some Common Cases',
     long_description=README,
     long_description_content_type='text/markdown',
-    
+
     url='https://github.com/harui2019/qurry/qurry',
     author='Huai-Chung Chang',
     author_email='harui2019@proton.me',
-    
+
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    
+
     install_requires=requirement,
     python_requires=">=3.9",
 )
