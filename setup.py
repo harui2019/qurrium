1584975f58156b9242c6f96b5097d2f3d63344c8
harui <harui2019@proton.me>
2023-04-04 19:03:08 +0800

    chore: setup.py update

--- setup.py
+++ setup.py
@@ -18,21 +18,19 @@ with open(README_PATH) as readme_file:
 
 qiskit_main = [
     # qiskit ibmq provider dedicated
-    # "requests~=2.28.0",
-    # "numpy<1.24",
-    # "python-dateutil==2.8.0",
-    # "requests-ntlm==1.1.0",
-    # "websocket-client>=1.5.1",
-    # "websockets==10.0 ; python_version>='3.7'",
-    # "websockets>=9.1 ; python_version<'3.7'",
-    # "dataclasses>=0.8 ; python_version<'3.7'",
+    "requests~=2.28.0",
+    "numpy<1.24",
+    "python-dateutil==2.8.0",
+    "requests-ntlm==1.1.0",
+    "websocket-client>=1.5.1",
+    "websockets==10.0 ; python_version>='3.7'",
+    "websockets>=9.1 ; python_version<'3.7'",
+    "dataclasses>=0.8 ; python_version<'3.7'",
     
     "qiskit==0.41.1",
     "qiskit-aer==0.11.2",
     "qiskit-ibm-provider==0.4.0",
     "qiskit-terra==0.23.2",
-    "requests~=2.28.0",
-    "urllib3~=1.26.0",
     "qiskit-ibmq-provider==0.20.1"
 ]
 qiskit_gpu = [
