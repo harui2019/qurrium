c5d4bba872b31a4658fcf08694d28fcf6d8d286e
harui <harui2019@proton.me>
2023-04-05 18:23:06 +0800

    chore(version): update version num to 6.0-beta

--- qurry/version.py
+++ qurry/version.py
@@ -1,9 +1,13 @@
 version_main = (0, 6, 0)
 version_beta = ('beta', 1)
+isBeta = True
+
+
 def _beta(beta_str: str, serial: int) -> str:
     return beta_str+str(serial).rjust(2, '0')
-isBeta = False
 
-__version__ = version_main + (_beta(*version_beta), ) if isBeta else version_main
+
+__version__ = version_main + \
+    (_beta(*version_beta), ) if isBeta else version_main
 __version_str__ = '.'.join(map(str, version_main)) + (
     '-'+_beta(*version_beta) if isBeta else '')
