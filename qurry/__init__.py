ebdf7a3560a2932a87da890e0180634b6d0a06a6
harui <harui2019@proton.me>
2023-04-10 05:38:39 +0800

    feat(qurmagsq): #35 #39 #33 - Freeze `qurstrop`

--- qurry/__init__.py
+++ qurry/__init__.py
@@ -1,7 +1,7 @@
 from .qurmagsq import MagnetSquare
 from .qurrech import EchoListen
 from .qurrent import EntropyMeasure
-from .qurstrop import StringOperator
+# from .qurstrop import StringOperator
 from .qurrium import QurryV5 as Qurry
 
 from .tools import (
--- qurry/qurstrop/__init__.py
+++ qurry/qurstrop/__init__.py
@@ -1,25 +1,25 @@
-from typing import Literal
+# from typing import Literal
 
-from .qurstrop import StringOperatorV3 as StringOperatorOrigin
+# from .qurstrop import StringOperatorV3 as StringOperatorOrigin
 
 
-def StringOperator(
-    *args,
-    method: Literal['original'] = 'original',
-    **kwargs,
-) -> StringOperatorOrigin:
-    """Call `StringOperator` methods.
+# def StringOperator(
+#     *args,
+#     method: Literal['original'] = 'original',
+#     **kwargs,
+# ) -> StringOperatorOrigin:
+#     """Call `StringOperator` methods.
 
-    Args:
-        method (Literal[&#39;original&#39;], optional): 
+#     Args:
+#         method (Literal[&#39;original&#39;], optional): 
 
-            - original: the original `StringOperator`.
-            Defaults to 'original'.
+#             - original: the original `StringOperator`.
+#             Defaults to 'original'.
 
-    Returns:
-        StringOperatorOrigin: method.
-    """
-    if method == 'original':
-        return StringOperatorOrigin(*args, **kwargs)
-    else:
-        return StringOperatorOrigin(*args, **kwargs)
+#     Returns:
+#         StringOperatorOrigin: method.
+#     """
+#     if method == 'original':
+#         return StringOperatorOrigin(*args, **kwargs)
+#     else:
+#         return StringOperatorOrigin(*args, **kwargs)
