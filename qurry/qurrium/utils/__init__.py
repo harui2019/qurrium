20c4a1e0d0a6f5ac79bfb3462134d9ab865fa14a
harui <harui2019@proton.me>
2023-04-19 05:18:02 +0800

    feat(util.randomize): - Remove `HaarBase` #35 - Add `cycling_slice`

--- qurry/qurrium/utils/__init__.py
+++ qurry/qurrium/utils/__init__.py
@@ -2,8 +2,8 @@ from .construct import qubit_selector, wave_selector, decomposer, get_counts, wo
 from .inputfixer import levenshtein_distance
 from .randomized import (
     RXmatrix, RYmatrix, RZmatrix,
-    makeTwoBitStr, makeTwoBitStrOneLiner, haarBase,
-    hamming_distance, ensembleCell, densityMatrixToBloch, qubitOpToPauliCoeff
+    makeTwoBitStr, makeTwoBitStrOneLiner, cycling_slice,
+    hamming_distance, ensembleCell, densityMatrixToBloch, qubitOpToPauliCoeff,
 )
 from .iocontrol import naming, IOComplex, FULL_SUFFIX_OF_COMPRESS_FORMAT, STAND_COMPRESS_FORMAT
 from .datetime import currentTime, datetimeDict
