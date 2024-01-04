d1c47dac573daeb156ec7f32f2e126c334db1454
harui2019 <harui2019@proton.me>
2024-01-04 17:33:18 +0800

    fix(recipe): shorten import path

--- qurry/recipe/__init__.py
+++ qurry/recipe/__init__.py
@@ -6,3 +6,12 @@ Position for `qurry.recipe`
 See https://github.com/harui2019/qurry/issues/82
 
 """
+from .simple import (
+    Intracell,
+    Singlet,
+    TrivialParamagnet,
+    TopologicalParamagnet,
+    Cluster,
+    Cat,
+    GHZ,
+)
