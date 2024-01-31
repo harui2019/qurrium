59a03a30e91bc2b04d154e6a2e7a9ce131543a9b
harui2019 <harui2019@proton.me>
2024-02-01 03:45:55 +0800

    refactor(qurrent): - randomized_measure refined

---
+++ qurry/qurrent/randomized_measure/__init__.py
@@ -0,0 +1,11 @@
+"""
+===========================================================
+Second Renyi Entropy - Randomized Measurement 
+(:mod:`qurry.qurrent.randomized_measure`)
+===========================================================
+
+"""
+
+from .experiment import EntropyRandomizedExperiment
+from .analysis import EntropyRandomizedAnalysis
+from .qurry import EntropyRandomizedMeasure
