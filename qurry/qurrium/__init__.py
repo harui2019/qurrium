025bb730e1cf570067af247f9277ba32dbff1a7e
harui2019 <harui2019@proton.me>
2024-02-01 03:48:41 +0800

    refactor(qurrium): - Cancel import point for `QurryPrototype`, `ExperimentPrototype`, `ArgumentsPrototype`, `AnalysisPrototype`

--- qurry/qurrium/__init__.py
+++ qurry/qurrium/__init__.py
@@ -5,9 +5,6 @@ Qurrium (:mod:`qurry.qurry.qurrium`)
 
 """
 
-from .qurrium import QurryPrototype
-from .experiment import ExperimentPrototype, ArgumentsPrototype
-from .analysis import AnalysisPrototype
 from .samplingqurry import QurryV5 as SamplingExecuter
 from .wavesqurry import WavesExecuter
 
