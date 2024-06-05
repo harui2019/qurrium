c837e985ee94f8fd984ca9b42fc15418ce673e2d
harui2019_action_bot <harui2019@users.noreply.github.com>
2024-06-06 04:00:04 +0800

    feat(process): remake magnet square

---
+++ qurry/process/magnet_square/magnet_square.py
@@ -0,0 +1,69 @@
+"""
+================================================================
+Postprocessing - Magnet Square - Magnet Square
+(:mod:`qurry.process.magnet_square.magnet_square`)
+================================================================
+
+"""
+
+from typing import Union, Optional, TypedDict
+import numpy as np
+import tqdm
+
+from ..availability import (
+    PostProcessingBackendLabel,
+)
+from .magsq_core import magnetic_square_core, DEFAULT_PROCESS_BACKEND
+
+
+class MagnetSquare(TypedDict):
+    """Magnet Square type."""
+
+    magnet_square: Union[float, np.float64]
+    """Magnet Square."""
+    magnet_square_cells: dict[int, Union[float, np.float64]]
+    """Magnet Square cells."""
+    countsNum: int
+    """Number of counts."""
+    takingTime: float
+    """Taking time."""
+
+
+def magnet_square(
+    shots: int,
+    counts: list[dict[str, int]],
+    num_qubits: int,
+    backend: PostProcessingBackendLabel = DEFAULT_PROCESS_BACKEND,
+    workers_num: Optional[int] = None,
+    pbar: Optional[tqdm.tqdm] = None,
+) -> MagnetSquare:
+    """Calculate the magnet square.
+
+    Args:
+        shots (int): Number of shots.
+        counts (list[dict[str, int]]): List of counts.
+        num_qubits (int): Number of qubits.
+        backend (Optional[PostProcessingBackendLabel], optional): Backend to use. Defaults to None.
+        workers_num (Optional[int], optional): Number of workers. Defaults to None.
+        pbar (Optional[tqdm.tqdm], optional): Progress bar. Defaults to None.
+
+    Returns:
+        MagnetSquare: Magnet Square.
+    """
+    if isinstance(pbar, tqdm.tqdm):
+        pbar.set_description("Magnet Square being calculated.")
+    (magsq, magnet_square_cells, counts_num, taking_time, _msg) = magnetic_square_core(
+        shots=shots,
+        counts=counts,
+        num_qubits=num_qubits,
+        backend=backend,
+        multiprocess_pool_size=workers_num,
+    )
+    if isinstance(pbar, tqdm.tqdm):
+        pbar.set_description(f"Magnet Square calculated in {taking_time} seconds.")
+    return {
+        "magnet_square": magsq,
+        "magnet_square_cells": magnet_square_cells,
+        "countsNum": counts_num,
+        "takingTime": taking_time,
+    }
--- qurry/process/status/backend.py
+++ qurry/process/status/backend.py
@@ -6,6 +6,7 @@ Post-processing Status Conclusions
 """
 
 from typing import Union
+
 from ..randomized_measure import (
     entangled_availability,
     purity_cell_availability,
@@ -13,6 +14,8 @@ from ..randomized_measure import (
     echo_cell_availability,
 )
 from ..hadamard_test import purity_echo_core_availability
+from ..magnet_square import magnet_square_availability
+
 from ..utils import construct_availability, randomized_availability
 from ..availability import BACKEND_TYPES
 from ...version import __version__
@@ -34,6 +37,7 @@ def availability_status_print() -> (
         randomized_availability,
         construct_availability,
         purity_echo_core_availability,
+        magnet_square_availability,
     ]
     pre_hoshi = [
         ("txt", f"| Qurry version: {__version__}"),
