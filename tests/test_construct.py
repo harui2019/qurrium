25dfe8e586dfc026cd2ef47ac7a36e93cc10438d
harui2019_action_bot <harui2019@users.noreply.github.com>
2024-06-18 14:11:27 +0800

    ci(pytest): More accurate test - Add qurrent counts_used implementation check. - Now every assertion has error msg.

--- tests/test_construct.py
+++ tests/test_construct.py
@@ -4,9 +4,12 @@ Test the qurry.boorust module.
 ============================================================================
 
 """
+
 from typing import Union
 import pytest
 from qurry.process.utils.randomized import (
+    RUST_AVAILABLE as rust_available_randomized,
+    CYTHON_AVAILABLE as cython_available_randomized,
     cycling_slice as cycling_slice_py,
     cycling_slice_cy,
     cycling_slice_rust,
@@ -34,31 +37,32 @@ test_setup_cycling: list[tuple[Union[int, tuple[int, int]], str]] = []
 def test_qubit_selector(test_items: tuple[int, Union[int, tuple[int, int]], str]):
     """Test the qubit_selector function."""
 
+    assert rust_available_construct, "Rust is not available."
     qubit_selector_py_result = qubit_selector_py(*test_items[:1])
     qubit_selector_rust_result = qubit_selector_rust(*test_items[:1])
 
-    if rust_available_construct:
-        assert qubit_selector_rust_result == qubit_selector_py_result, (
-            "Rust and Python results are not equal in"
-            + f"qubit_selector at case: {test_items[2]}."
-        )
+    assert qubit_selector_rust_result == qubit_selector_py_result, (
+        "Rust and Python results are not equal in"
+        + f"qubit_selector at {test_items[2]}: {test_items[0]} qubits {test_items[1]}."
+    )
 
     selected = qubit_selector_py_result
 
+    assert cython_available_randomized, "Cython is not available."
+    assert rust_available_randomized, "Rust is not available."
     cycling_slice_cy_result = cycling_slice_cy("01234567", *selected, 1)
     cycling_slice_py_result = cycling_slice_py("01234567", *selected, 1)
     cycling_slice_rust_result = cycling_slice_rust("01234567", *selected, 1)
 
     assert cycling_slice_cy_result == cycling_slice_py_result, (
         "Cython and Python results are not equal in"
-        + f"cycling_slice at case: {test_items[1]}."
+        + f"cycling_slice at {test_items[2]}: {test_items[0]} qubits {test_items[1]}."
     )
-    if rust_available_construct:
-        assert cycling_slice_rust_result == cycling_slice_py_result, (
-            "Rust and Python results are not equal in"
-            + f"cycling_slice at case: {test_items[1]}."
-        )
-        assert cycling_slice_rust_result == cycling_slice_cy_result, (
-            "Rust and Cython results are not equal in"
-            + f"cycling_slice at case: {test_items[1]}."
-        )
+    assert cycling_slice_rust_result == cycling_slice_py_result, (
+        "Rust and Python results are not equal in"
+        + f"cycling_slice at {test_items[2]}: {test_items[0]} qubits {test_items[1]}."
+    )
+    assert cycling_slice_rust_result == cycling_slice_cy_result, (
+        "Rust and Cython results are not equal in"
+        + f"cycling_slice at {test_items[2]}: {test_items[0]} qubits {test_items[1]}."
+    )
--- tests/test_qurrent.py
+++ tests/test_qurrent.py
@@ -4,6 +4,7 @@ Test the qurry.qurrent module EntropyMeasure class.
 ================================================================
 
 """
+
 import pytest
 import numpy as np
 from qurry.qurrent import EntropyMeasure
@@ -14,22 +15,22 @@ from qurry.recipe import TrivialParamagnet, GHZ, TopologicalParamagnet
 tag_list = mori.TagList()
 statesheet = hoshi.Hoshi()
 
-expDemo01 = EntropyMeasure(method="hadamard")
-expDemo02 = EntropyMeasure(method="randomized")
+exp_method_01 = EntropyMeasure(method="hadamard")
+exp_method_02 = EntropyMeasure(method="randomized")
 
 
 wave_adds_01 = []
 wave_adds_02 = []
 
 for i in range(4, 7, 2):
-    wave_adds_01.append(expDemo01.add(TrivialParamagnet(i), f"{i}-trivial"))
-    wave_adds_02.append(expDemo02.add(TrivialParamagnet(i), f"{i}-trivial"))
+    wave_adds_01.append(exp_method_01.add(TrivialParamagnet(i), f"{i}-trivial"))
+    wave_adds_02.append(exp_method_02.add(TrivialParamagnet(i), f"{i}-trivial"))
 
-    wave_adds_01.append(expDemo01.add(GHZ(i), f"{i}-GHZ"))
-    wave_adds_02.append(expDemo02.add(GHZ(i), f"{i}-GHZ"))
+    wave_adds_01.append(exp_method_01.add(GHZ(i), f"{i}-GHZ"))
+    wave_adds_02.append(exp_method_02.add(GHZ(i), f"{i}-GHZ"))
 
-    wave_adds_01.append(expDemo01.add(TopologicalParamagnet(i), f"{i}-topological"))
-    wave_adds_02.append(expDemo02.add(TopologicalParamagnet(i), f"{i}-topological"))
+    wave_adds_01.append(exp_method_01.add(TopologicalParamagnet(i), f"{i}-topological"))
+    wave_adds_02.append(exp_method_02.add(TopologicalParamagnet(i), f"{i}-topological"))
 
 backend = GeneralSimulator()
 # backend = BasicAer.backends()[0]
@@ -44,11 +45,15 @@ def test_quantity_01(tgt):
         tgt (Hashable): The target wave key in Qurry.
     """
 
-    exp_id = expDemo01.measure(wave=tgt, backend=backend)
-    expDemo01.exps[exp_id].analyze()
-    quantity = expDemo01.exps[exp_id].reports[0].content._asdict()
-    assert all(["entropy" in quantity, "purity" in quantity])
-    assert np.abs(quantity["purity"] - 1.0) < 1e-0
+    exp_id = exp_method_01.measure(wave=tgt, backend=backend)
+    exp_method_01.exps[exp_id].analyze()
+    quantity = exp_method_01.exps[exp_id].reports[0].content._asdict()
+    assert all(
+        ["entropy" in quantity, "purity" in quantity]
+    ), f"The necessary quantities 'entropy', 'purity' are not found: {quantity.keys()}."
+    assert (
+        np.abs(quantity["purity"] - 1.0) < 1e-0
+    ), f"The hadamard test result is wrong: {np.abs(quantity['purity'] - 1.0)} !<= < 1e-0."
 
 
 @pytest.mark.parametrize("tgt", wave_adds_02)
@@ -59,7 +64,16 @@ def test_quantity_02(tgt):
         tgt (Hashable): The target wave key in Qurry.
     """
 
-    exp_id = expDemo02.measure(wave=tgt, times=10, backend=backend)
-    expDemo02.exps[exp_id].analyze(2)
-    quantity = expDemo02.exps[exp_id].reports[0].content._asdict()
-    assert all(["entropy" in quantity, "purity" in quantity])
+    exp_id = exp_method_02.measure(wave=tgt, times=10, backend=backend)
+    analysis_01 = exp_method_02.exps[exp_id].analyze(2)
+    quantity_01 = analysis_01.content._asdict()
+    analysis_02 = exp_method_02.exps[exp_id].analyze(2, counts_used=range(5))
+    quantity_02 = analysis_02.content._asdict()
+    assert all(
+        ["entropy" in quantity_01, "purity" in quantity_01]
+    ), f"The necessary quantities 'entropy', 'purity' are not found: {quantity_01.keys()}."
+    assert quantity_02["entropyAllSys"] != quantity_01["entropyAllSys"], (
+        "The all system entropy is not changed: "
+        + f"counts_used: {quantity_01['counts_used']}: {quantity_02['entropyAllSys']}, "
+        + f"counts_used: {quantity_02['counts_used']}: {quantity_02['entropyAllSys']},"
+    )
