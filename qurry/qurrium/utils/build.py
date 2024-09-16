fd01da3a714b08c964b7c134fa48e3c75bf90f50
harui2019 <harui2019@proton.me>
2024-09-16 21:06:28 +0800

    feat(qurrium.utils): new module `build`

---
+++ qurry/qurrium/utils/build.py
@@ -0,0 +1,81 @@
+"""
+===========================================================
+Build Tools
+(:mod:`qurry.qurry.qurrium.utils.build`)
+===========================================================
+"""
+
+from typing import Union, Optional
+from collections.abc import Hashable
+from qiskit import QuantumCircuit
+from qiskit.transpiler.passmanager import PassManager
+from ..container.waves_static import WaveContainer
+
+
+def passmanager_processor(
+    passmanager: Union[str, PassManager, tuple[str, PassManager], None],
+    passmanager_container: dict[str, PassManager],
+) -> Optional[tuple[str, PassManager]]:
+    """Process the passmanager for Qurrium.
+
+    Args:
+        passmanager (Union[str, PassManager, tuple[str, PassManager], None]): The passmanager.
+        passmanager_container (dict[str, PassManager]): The container of passmanager.
+
+    Raises:
+        KeyError: If the passmanager not found in the container.
+        ValueError: If the passmanager is invalid.
+
+    Returns:
+        Optional[tuple[str, PassManager]]: The passmanager pair.
+    """
+    if isinstance(passmanager, str):
+        if passmanager not in passmanager_container:
+            raise KeyError(f"Passmanager '{passmanager}' not found in {passmanager_container}")
+        passmanager_pair = passmanager, passmanager_container[passmanager]
+    elif isinstance(passmanager, PassManager):
+        passmanager_pair = f"pass_{len(passmanager_container)}", passmanager
+        passmanager_container[passmanager_pair[0]] = passmanager_pair[1]
+    elif isinstance(passmanager, tuple):
+        if not isinstance(passmanager[1], PassManager) or not isinstance(passmanager[0], str):
+            raise ValueError(f"Invalid passmanager: {passmanager}")
+        passmanager_pair = passmanager
+        passmanager_container[passmanager_pair[0]] = passmanager_pair[1]
+    elif passmanager is None:
+        passmanager_pair = None
+    else:
+        raise ValueError(f"Invalid passmanager: {passmanager}")
+    return passmanager_pair
+
+
+def circuits_processor(
+    circuits: list[Union[QuantumCircuit, Hashable]],
+    wave_container: WaveContainer,
+) -> dict[Hashable, QuantumCircuit]:
+    """Process the circuits for Qurrium.
+
+    Args:
+        circuits (list[Union[QuantumCircuit, Hashable]]): The circuits.
+        wave_container (WaveContainer): The container of waves.
+
+    Raises:
+        KeyError: If the wave not found in the container.
+        ValueError: If the circuit is invalid.
+
+    Returns:
+        dict[Hashable, QuantumCircuit]: The circuits maps.
+    """
+
+    circuits_maps = {}
+    for _circuit in circuits:
+        if isinstance(_circuit, QuantumCircuit):
+            key = wave_container.add(wave=_circuit)
+            circuits_maps[key] = wave_container[key]
+        elif isinstance(_circuit, Hashable):
+            if wave_container.has(_circuit):
+                circuits_maps[_circuit] = wave_container[_circuit]
+            else:
+                raise KeyError(f"Wave {_circuit} not found in {wave_container}")
+        else:
+            raise ValueError(f"Invalid type of circuit: {_circuit}, type: {type(_circuit)}")
+    return circuits_maps
