47f6762e119e857fdf33a240ecc97b8594ada39f
harui2019 <harui2019@proton.me>
2024-09-20 19:48:29 +0800

    feat(qurrium): rename import

--- qurry/qurrium/__init__.py
+++ qurry/qurrium/__init__.py
@@ -5,7 +5,7 @@ Qurrium (:mod:`qurry.qurry.qurrium`)
 
 """
 
-from .samplingqurry import QurryV5 as SamplingExecuter
+from .samplingqurry import SamplingQurry
 from .wavesqurry import WavesExecuter
 
 from .utils import decomposer
