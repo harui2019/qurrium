f5cd43a158de01d2d92ecbe1ac2a236ef9d9dfdb
harui2019 <harui2019@proton.me>
2024-10-07 15:12:34 +0800

    feat(boost): [Acceleration] It's time to say goodbye to Cython for building issues #198 - Remove setup.py

--- setup-no-abi3.py
+++ setup-no-abi3.py
@@ -6,26 +6,8 @@ Setup Script for Qurry
 """
 
 import os
-from setuptools import setup, Extension
+from setuptools import setup
 from setuptools_rust import Binding, RustExtension
-
-
-def re_cythonize(extensions, **kwargs) -> list[Extension]:
-    """Re-cythonize the extensions.
-    To optionally use cython.
-    """
-    try:
-        # pylint: disable=import-outside-toplevel
-        from Cython.Build import cythonize
-
-        # pylint: enable=import-outside-toplevel
-
-        return cythonize(extensions, **kwargs)
-    except ImportError:
-        print("| Cython is not installed.")
-        print("| Please install cython manually at first,")
-        print("| Then reinstall qurry for more powerful performance.")
-        return []
 
 
 rust_extension = [
@@ -37,11 +19,6 @@ rust_extension = [
     )
 ]
 
-cy_extensions = [
-    Extension("qurry.boost.randomized", ["qurry/boost/randomized.pyx"]),
-    Extension("qurry.boost.inputfixer", ["qurry/boost/inputfixer.pyx"]),
-]
-
 with open(os.path.join("qurry", "VERSION.txt"), encoding="utf-8") as version_file:
     __version__ = version_file.read().strip()
 
@@ -50,10 +27,6 @@ print(f"| Version: {__version__}")
 
 setup(
     version=__version__,
-    ext_modules=re_cythonize(
-        cy_extensions,
-        language_level=3,
-    ),
     rust_extensions=rust_extension,
     # options={"bdist_wheel": {"py_limited_api": "cp39"}},
 )
