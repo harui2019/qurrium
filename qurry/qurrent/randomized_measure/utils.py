7b2191e63c0e8af4e522fcf5f1d1bfa66bd26c16
harui2019 <harui2019@proton.me>
2024-10-16 20:31:58 +0800

    feat(qurrent): deprecated choose meaure range - Now always measure all - circuit method

--- qurry/qurrent/randomized_measure/utils.py
+++ qurry/qurrent/randomized_measure/utils.py
@@ -133,3 +133,49 @@ def circuit_method_core(
         qc_exp1.measure(q_func1[j], c_meas1[j - measure[0]])
 
     return qc_exp1
+
+
+def circuit_method_core_2(
+    idx: int,
+    target_circuit: QuantumCircuit,
+    target_key: Hashable,
+    exp_name: str,
+    unitary_loc: tuple[int, int],
+    unitary_sublist: dict[int, Operator],
+) -> QuantumCircuit:
+    """Build the circuit for the experiment.
+
+    Args:
+        idx (int): Index of the randomized unitary.
+        target_circuit (QuantumCircuit): Target circuit.
+        target_key (Hashable): Target key.
+        exp_name (str): Experiment name.
+        unitary_loc (tuple[int, int]): Unitary operator location.
+        unitary_sublist (dict[int, Operator]): Unitary operator list.
+
+    Returns:
+        QuantumCircuit: The circuit for the experiment.
+    """
+
+    num_qubits = target_circuit.num_qubits
+    old_name = "" if isinstance(target_circuit.name, str) else target_circuit.name
+
+    q_func1 = QuantumRegister(num_qubits, "q1")
+    c_meas1 = ClassicalRegister(num_qubits, "c1")
+    qc_exp1 = QuantumCircuit(q_func1, c_meas1)
+    qc_exp1.name = (
+        f"{exp_name}_{idx}" + ""
+        if len(str(target_key)) < 1
+        else f".{target_key}" + "" if len(old_name) < 1 else f".{old_name}"
+    )
+
+    qc_exp1.compose(target_circuit, [q_func1[i] for i in range(num_qubits)], inplace=True)
+
+    qc_exp1.barrier()
+    for j in range(*unitary_loc):
+        qc_exp1.append(unitary_sublist[j].to_instruction(), [j])
+
+    for j in range(num_qubits):
+        qc_exp1.measure(q_func1[j], c_meas1[j])
+
+    return qc_exp1
