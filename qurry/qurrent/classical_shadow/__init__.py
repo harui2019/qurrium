5746c654d3abedf2299404d573c97d28e4e06479
harui2019 <harui2019@proton.me>
2024-11-11 18:09:39 +0800

    feat(qurshady): init

---
+++ qurry/qurrent/classical_shadow/__init__.py
@@ -0,0 +1,12 @@
+"""
+================================================================
+ShadowUnveil - 
+Classical Shadow with The Results of Second Order Renyi Entropy
+(:mod:`qurry.qurrent.classical_shadow`)
+================================================================
+
+"""
+
+# from .analysis import ShadowUnveilAnalysis
+# from .experiment import ShadowUnveilExperiment
+# from .qurry import ShadowUnveilHadamard
---
+++ qurry/qurrent/classical_shadow/qurry.py
@@ -0,0 +1,13 @@
+"""
+===========================================================
+ShadowUnveil - Qurry
+(:mod:`qurry.qurrent.classical_shadow.qurry`)
+===========================================================
+
+"""
+
+from ...qurrium.qurrium import QurriumPrototype
+
+
+class ShadowUnveil(QurriumPrototype):
+    """Classical Shadow with The Results of Second Order Renyi Entropy."""
